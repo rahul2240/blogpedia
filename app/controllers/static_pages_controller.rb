6e73ca35a4ceca37223809a27b4ccd23649a3468
rahul <rahulsingh2240@gmail.com>
2017-07-09 19:12:43 +0530

    Front end ready

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,19 @@
 class StaticPagesController < ApplicationController
+  
+  
+  	before_action :set_post, only: [:show, :edit, :update, :destroy]
+
+  # GET /posts
+  # GET /posts.json
   def home
+    @posts = Post.all
   end
+
+  # GET /posts/1
+  # GET /posts/1.json
+  def show
+  end
+
 
   def help
   end
@@ -10,4 +23,13 @@ class StaticPagesController < ApplicationController
 
   def contact
   end
+
+  private
+
+    # Use callbacks to share common setup or constraints between actions.
+    def set_post
+      @post = Post.friendly.find(params[:id])
+    end
+
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  resources :posts
   root 'static_pages#home'
 
   get '/help', to: 'static_pages#help'
@@ -7,6 +8,8 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
 
   get '/contact', to: 'static_pages#contact'
+
+  get '/show/:id', to: 'static_pages#show', as: 'show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
