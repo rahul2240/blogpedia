e5aa063c9919c5dd446de053d55f3f25d2688422
rahul <rahulsingh2240@gmail.com>
2017-07-10 16:47:15 +0530

    Login Finsihed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,12 @@
 class Post < ApplicationRecord
-
+	belongs_to :user
+	
 	extend FriendlyId
 	friendly_id :title, use: :slugged
 
 	scope :most_recent, -> {order(id: :desc)}
 
-	belongs_to :user
+	
 	validates :user_id,presence: true
     validates :title, presence: true, length: { maximum: 50 }
     validates :description, presence: true
